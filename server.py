b0f4ec824da57ca5b8bdf21a0d2bc1289b6cbd6d
rejirejireji <reiji.01101114@gmail.com>
2024-01-26 10:41:41 +0900

    APIkeyの読み込みは完了した状態

--- server.py
+++ server.py
@@ -2,10 +2,11 @@ import os
 from dotenv import load_dotenv
 from openai import OpenAI
 
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
-
 # .env ファイルから環境変数を読み込む
 load_dotenv()
+
+client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+
 
 def generate_ad_keywords(base_keyword):
 
